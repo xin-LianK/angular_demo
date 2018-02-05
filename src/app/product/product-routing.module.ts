503a09207412e16c718887237451e5c61d886c81
xin-LianK <350824110@qq.com>
2018-02-05 15:06:40 +0800

    -

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
       {
         path: 'category',
         component: CategoryComponent
+      },
+      {
+        path: '',
+        redirectTo: 'index',
       }
     ]
   },
