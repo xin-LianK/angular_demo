59c8573e995971f1236be295fa47f695c3ce91f9
xin-LianK <350824110@qq.com>
2018-02-06 10:08:02 +0800

    material半成功

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,104 +5,26 @@ import { Router } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationBuilder } from '@angular/platform-browser/animations/src/animation_builder';
 
-import '../polyfills';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatStepperModule,
-} from '@angular/material';
-import { TabsTemplateLabelExampleComponent } from './tabs-template-label-example';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http';
-import { CdkTableModule } from '@angular/cdk/table';
-
-NgModule({
-  exports: [
-    CdkTableModule,
-    MatAutocompleteModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatStepperModule,
-    MatDatepickerModule,
-    MatDialogModule,
-    MatExpansionModule,
-    MatGridListModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatNativeDateModule,
-    MatPaginatorModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatRippleModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
-    MatSortModule,
-    MatTableModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
-  ]
-})
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabsTemplateLabelExampleComponent
-],
+  ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatCheckboxModule,
+    BrowserModule,
     FormsModule,
     HttpModule,
-    HttpClientModule,
-    MatNativeDateModule,
-    ReactiveFormsModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent, TabsTemplateLabelExampleComponent],
-  entryComponents: [TabsTemplateLabelExampleComponent],
+  bootstrap: [AppComponent],
+  entryComponents: [],
 })
 export class AppModule { }
--- src/app/last/last.module.ts
+++ src/app/last/last.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LastComponent } from './last.component';
 import { IndexComponent } from './index/index.component';
 import { LastRoutingModule } from './last.routing';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    LastRoutingModule
+    LastRoutingModule,
+    MaterialModule
   ],
   declarations: [LastComponent,
     IndexComponent
