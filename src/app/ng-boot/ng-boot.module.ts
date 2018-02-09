7a29ac74e5a1b1eab735aba1e0aff3e71f67f6ff
xin-LianK <350824110@qq.com>
2018-02-09 17:41:18 +0800

    ng-bootstrap配置

--- src/app/ng-boot/ng-boot.module.ts
+++ src/app/ng-boot/ng-boot.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NgBootComponent } from './ng-boot.component';
 import { IndexComponent } from './index/index.component';
 import { NgBootRoutingModule } from './ng-boot.routing';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
-    NgBootRoutingModule
+    NgBootRoutingModule,
+    NgbModule
   ],
   declarations: [NgBootComponent,
     IndexComponent
