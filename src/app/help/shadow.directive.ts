3c99a3ac8d9a077bbac2e8e02e473e447f123c93
xin-LianK <350824110@qq.com>
2018-02-26 16:47:16 +0800

    升级版的自定义指令

--- src/app/help/shadow.directive.ts
+++ src/app/help/shadow.directive.ts
@@ -1,12 +1,20 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appShadow]'
 })
-export class ShadowDirective {
+export class ShadowDirective implements OnInit {
 
+  @Input() appShadow: string;
+  @Input() appShadowX: string;
+  @Input() appShadowY: string;
+  @Input() appShadowBlur: string;
   constructor(private ele: ElementRef, private renderer: Renderer2) {
-    renderer.setStyle(ele.nativeElement, 'box-shadow', '2px 2px 12px #58A362')
+    // renderer.setStyle(ele.nativeElement, 'box-shadow', '2px 2px 12px #58A362')
+  }
+  ngOnInit() {
+    let shadowStr = `${this.appShadowX} ${this.appShadowY} ${this.appShadowBlur} ${this.appShadow}`;
+    this.renderer.setStyle(this.ele.nativeElement, 'box-shadow', shadowStr);
   }
 
 }
