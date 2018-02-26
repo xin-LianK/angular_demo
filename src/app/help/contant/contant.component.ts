9125a173e01fd7dda83c0ed696d8f63c8ce69db3
xin-LianK <350824110@qq.com>
2018-02-26 15:57:07 +0800

    --针对不同的参数 需要在括号里声明原来的参数针对不同的参数 需要在括号里声明原来的参数

--- src/app/help/contant/contant.component.ts
+++ src/app/help/contant/contant.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ContantComponent implements OnInit {
 
   @Input() story: string;
-  @Input('character') mainCharacter: string;
-  
+  @Input('character') mainCharacter: string;//针对不同的参数 需要在括号里声明原来的参数
+
   constructor() { }
 
   ngOnInit() {
