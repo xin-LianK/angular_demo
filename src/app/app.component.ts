497e63390590ae1498fb5500c078bd46e38e863e
xin-LianK <350824110@qq.com>
2018-02-28 17:48:41 +0800

    --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -60,7 +60,17 @@ export class AppComponent {
   getPath() {
     alert(this.location.path());
   }
-  goPath(){
+  // normalize 获取路径并返回标准化路径
+  getAllPath() {
+    alert(this.location.normalize(''))
+  }
+  // 更改给定的URL并将其添加到浏览器的历史记录
+  goPath() {
     this.location.go('https://www.baidu.com')
   }
+  // 更改为给定的URL，并替换历史记录中最顶端的URL。这使得如果用户返回，它将不会回到用户所在的URL，而是之前的那个
+  rePath() {
+    this.location.replaceState('https://www.google.com')
+  }
+  // isCurrentPathEqualTo 比较两个给定的路径值，看它们是否相等
 }
