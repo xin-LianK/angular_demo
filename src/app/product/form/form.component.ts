daabe5fe0470259ff5c49150c6433951b39d7076
xin-LianK <350824110@qq.com>
2018-03-05 15:25:46 +0800

    比较重要的属性绑定

--- src/app/product/form/form.component.ts
+++ src/app/product/form/form.component.ts
@@ -11,9 +11,34 @@ export class FormComponent implements OnInit {
   email: string;
   nickName: string;
   password: string;
+  publishedDate:'1920-12-31'
+  users: ['one', 'two', 'three', 'four'];
+  user = {
+    name: "Bob",
+    gender: 'm',
+    avatar: 'what',
+    newUser: 'Pick',
+    color:'lightbule',
+    preferences: {
+      nickName: true,
+      color: 'red'
+    },
+  }
+  setClasses() {
+    const classes = {
+      'has-avatar': this.user.avatar, 'new-user': this.user.newUser, bg: this.user.preferences.color
+    }; return classes;
+  }
+  userStyles = {
+    backgroundColor: 'papayawhip', border: '2px solid #666', width: '50%', margin: '0 auto', textAlign: 'center',
+    padding: '2rem'
+  };
   constructor() { }
 
   ngOnInit() {
+  }
+  printValue(){
+    console.log(1)
   }
   onSubmit(form: NgForm) {
     if (form.valid) {
